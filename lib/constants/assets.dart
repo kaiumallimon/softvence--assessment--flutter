d412542b8224eeca4bb1ef4294439da44f731cf8
Kaium Al Limon <limon.3cs@gmail.com>
2025-09-04 11:03:10 +0600

    added location page image

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -2,4 +2,5 @@ class AppAssets {
   static const String onboardingAsset1 = "assets/asset1.gif";
   static const String onboardingAsset2 = "assets/asset2.gif";
   static const String onboardingAsset3 = "assets/asset3.gif";
+  static const String locationAvatar = "assets/location_avatar.png";
 }
